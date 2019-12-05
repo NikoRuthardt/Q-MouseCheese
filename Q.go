221f929b2622baeca33729cb26adbbe6579f7879
Niko Ruthardt <buybypenny@googlemail.com>
2019-12-05 20:17:57 +0100

    added Q-learning

---
+++ Q.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"math"
+	"math/rand"
+	"time"
+)
+
+//State interface
+type State interface{}
+
+//Agent represents the Q learning Agent
+type Agent struct {
+	QTable map[State][]float64 // Q - Table
+	ε      float64             // exploration
+	/* to begin ε=1 */
+	α float64 // learning Rate
+	/* often used α= 0.1*/
+	γ float64 // discount factor
+	/*  A factor of 0 for γ will make the agent short sighted  */
+	actions int
+	/* space of all possible actions */
+}
+
+//NewAgent constructor for Q-Agent
+func NewAgent(actions int) *Agent {
+	return &Agent{
+		QTable:  map[State][]float64{},
+		ε:       1,
+		α:       0.2,
+		γ:       0.8,
+		actions: actions,
+	}
+}
+
+func (a *Agent) updateQ(state State, action int, reward float64, newState State) {
+	optimalFuture, _ := maxDir(a.getActions(newState))
+	/*** Q-Function ***/
+	/* new Q[s,a] = (1 - α) * Q[s,a] + α * (reward + γ * optimalFuture) */
+	a.QTable[state][action] = (1-a.α)*a.QTable[state][action] + a.α*(reward+a.γ*optimalFuture)
+}
+
+func (a *Agent) chooseAction(state State) (int, float64) {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	a.ε = math.Max(a.ε*0.999, a.ε*0.001) //let ε --> go slowly to  0
+
+	if rand.Float64() < a.ε {
+		return rand.Intn(a.actions), a.ε // choose random Action
+	}
+	actions := a.getActions(state) // choose action based on Q-Table
+	_, action := maxDir(actions)
+	return action, a.ε
+}
+
+func (a *Agent) getActions(state State) []float64 {
+	// if state exist -> return actions
+	if actions, ok := a.QTable[state]; ok {
+		return actions
+	}
+	// if state not exist -> create actions for state -> return actions
+	a.QTable[state] = make([]float64, a.actions)
+	return a.QTable[state]
+}
+
+func maxDir(actions []float64) (float64, int) {
+	// choose max "direction" from actions
+	max := actions[1] // -> 0 UP, 1 Down, 2 Right, 3 Left
+	index := 1
+	/* if all equal choose down */
+	for i, a := range actions {
+		if a > max {
+			max = a
+			index = i
+		}
+	}
+	return max, index
+
+}
--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"log"
-  "fmt"
+	"fmt"
 	"github.com/hajimehoshi/ebiten"
-  "github.com/hajimehoshi/ebiten/ebitenutil"
-  "github.com/hajimehoshi/ebiten/inpututil"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
+	"log"
 )
 
 const (
 	screenWidth  = 600
 	screenHeight = 600
-  tileSize = 100
+	tileSize     = 100
 )
 
 // Player
@@ -24,194 +24,226 @@ const (
 	down
 	left
 	right
-  none
+	none
 )
+
 type mouse struct {
-  row,col int
-  dir Dir
+	row, col int
+	dir      Dir
 }
 
-func (m *mouse) move(d Dir){
-  switch d {
-  case up:
-    m.col--
-  case down:
-    m.col++
-  case left:
-    m.row--
-  case right:
-    m.row++
-  }
+func (m *mouse) move(d Dir) {
+	switch d {
+	case up:
+		m.col--
+	case down:
+		m.col++
+	case left:
+		m.row--
+	case right:
+		m.row++
+	}
 }
 
+func (m *mouse) input(action int) (Dir, bool) {
+	if inpututil.IsKeyJustPressed(ebiten.KeyUp) || action == 0 {
+		if m.col == 0 {
+			return none, true
+		}
+		return up, true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyDown) || action == 1 {
+		if m.col == screenHeight/tileSize-1 {
+			return none, true
+		}
+		return down, true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyLeft) || action == 2 {
+		if m.row == 0 {
+			return none, true
+		}
+		return left, true
+	}
+	if inpututil.IsKeyJustPressed(ebiten.KeyRight) || action == 3 {
+		if m.row == screenWidth/tileSize-1 {
+			return none, true
+		}
+		return right, true
+	}
 
-func (m *mouse) input() (Dir, bool){
-    if inpututil.IsKeyJustPressed(ebiten.KeyUp){
-      if m.col == 0 {
-        return none,true
-      }
-      return up,true
-    }
-    if inpututil.IsKeyJustPressed(ebiten.KeyDown){
-      if m.col == screenHeight / tileSize -1 {
-        return none,true
-      }
-      return down,true
-    }
-    if inpututil.IsKeyJustPressed(ebiten.KeyLeft){
-      if m.row == 0 {
-        return none,true
-      }
-      return left,true
-    }
-    if inpututil.IsKeyJustPressed(ebiten.KeyRight){
-      if m.row == screenWidth / tileSize -1 {
-        return none,true
-      }
-      return right,true
-    }
-
-    return none, false
+	return none, false
 }
 
-func (m *mouse) updateState(grid Grid) {
-    nextTile := grid.getIndex(m.row, m.col)
-    switch grid.tiles[nextTile].value {
-    case 0:
-      return
-    case 1:
-      grid.tiles[nextTile].value = 0
-    case 2:
-      m.reset()
-    case 3:
-      m.reset()
-    }
+// reset --> end of episode else return reward
+func (m *mouse) updateState(grid Grid) int {
+	nextTile := grid.getIndex(m.row, m.col)
+	switch grid.tiles[nextTile].value {
+	case 0:
+		return 0
+	case 1:
+		grid.tiles[nextTile].value = 0
+    return 1
+	case 2:
+    m.reset()
+    return -100
+	case 3:
+    m.reset()
+    return 100
+	}
+  return 0
 }
 
-func (m *mouse) reset(){
-  m.col = 0
-  m.row = 0
-  initGrid()
+func (m *mouse) reset() {
+	m.col = 0
+	m.row = 0
+  episode++
+	initGrid()
 }
 
 // GameBoard
 
 // Grid the game board
-type Grid struct{
- tiles []Tile
+type Grid struct {
+	tiles []Tile
 }
 
-func (g *Grid) getIndex(row, col int) int{
-    for i, t := range g.tiles{
-        if t.cols == col && t.rows == row {
-          return i
-        }
-      }
-      return -1
+func (g *Grid) getIndex(row, col int) int {
+	for i, t := range g.tiles {
+		if t.cols == col && t.rows == row {
+			return i
+		}
+	}
+	return -1
 }
 
 // Tile :each point on the grid is a tile
 type Tile struct {
-  rows,cols,value int
+	rows, cols, value int
 }
 
-
-var(
-  tileImage, cheeseImage, trapImage, trippleCheeseImage, mouseImage *ebiten.Image
-  grid Grid
-  player mouse
+var (
+	tileImage, cheeseImage, trapImage, trippleCheeseImage, mouseImage *ebiten.Image
+	grid                                                              Grid
+	player                                                            mouse
+	agent                                                             *Agent
+  episode int
 )
 
+// load images
+func init() {
+	var err error
 
-// init Pictures
-func init(){
-  var err error
-
-  tileImage, _, _ = ebitenutil.NewImageFromFile("assets/tile_1x.png", ebiten.FilterDefault)
-  cheeseImage, _, _ = ebitenutil.NewImageFromFile("assets/cheese_1x.png", ebiten.FilterDefault)
-  trippleCheeseImage, _, _ = ebitenutil.NewImageFromFile("assets/trippleCheese_1x.png", ebiten.FilterDefault)
-  trapImage, _, _ = ebitenutil.NewImageFromFile("assets/trap_1x.png", ebiten.FilterDefault)
-
-  mouseImage, _, _ = ebitenutil.NewImageFromFile("assets/mouse_1x.png", ebiten.FilterDefault)
-
+	tileImage, _, err = ebitenutil.NewImageFromFile("assets/tile_1x.png", ebiten.FilterDefault)
   if err != nil {
     log.Fatal(err)
   }
+
+  cheeseImage, _, err = ebitenutil.NewImageFromFile("assets/cheese_1x.png", ebiten.FilterDefault)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  trippleCheeseImage, _, err = ebitenutil.NewImageFromFile("assets/trippleCheese_1x.png", ebiten.FilterDefault)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  trapImage, _, err = ebitenutil.NewImageFromFile("assets/trap_1x.png", ebiten.FilterDefault)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  mouseImage, _, err = ebitenutil.NewImageFromFile("assets/mouse_1x.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 // gameLoop
 func update(screen *ebiten.Image) error {
+  // actual State (State = tileIndex)
+  state := grid.getIndex(player.row, player.col)
 
-  if dir,input := player.input(); input {
-    player.move(dir)
-    player.updateState(grid)
-  }
+  // agent choose and make action (epsillon only for debug)
+  action, eps := agent.chooseAction(state)
+  dir,_ := player.input(action)
+	player.move(dir)
 
-  w, h := tileImage.Size()
+  //measure Reward
+  reward := player.updateState(grid)
+  newState := grid.getIndex(player.row, player.col)
+
+  // Update Q
+  agent.updateQ(state, action, float64(reward), newState)
+
 
   // Draw Grid
-  for _, t := range grid.tiles{
-    op := &ebiten.DrawImageOptions{}
-    op.GeoM.Translate(float64(t.rows * h),float64(t.cols*w))
-    screen.DrawImage(tileImage, op)
+	w, h := tileImage.Size()
 
-    switch t.value {
-    case 1:
-      screen.DrawImage(cheeseImage, op)
-    case 2:
-      screen.DrawImage(trapImage, op)
-    case 3:
-      screen.DrawImage(trippleCheeseImage, op)
-    }
-    // ebitenutil.DebugPrintAt(screen, fmt.Sprintf("%v", i), t.rows * h + 20, t.cols*w +20)
-  }
-  // draw Player
-  plOp := &ebiten.DrawImageOptions{}
-  plOp.GeoM.Translate(float64(player.row * h),float64(player.col*w))
-  screen.DrawImage(mouseImage,plOp)
+	for _, t := range grid.tiles {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Translate(float64(t.rows*h), float64(t.cols*w))
+		screen.DrawImage(tileImage, op)
 
-  ebitenutil.DebugPrintAt(screen, fmt.Sprintf("row:%v col:%v", player.row, player.col), 0,0)
+		switch t.value {
+		case 1:
+			screen.DrawImage(cheeseImage, op)
+		case 2:
+			screen.DrawImage(trapImage, op)
+		case 3:
+			screen.DrawImage(trippleCheeseImage, op)
+		}
 
-  if ebiten.IsDrawingSkipped() {
-  	return nil
-  }
+	}
 
-  return nil
+	// draw Player
+	opPlayer := &ebiten.DrawImageOptions{}
+	opPlayer.GeoM.Translate(float64(player.row*h), float64(player.col*w))
+	screen.DrawImage(mouseImage, opPlayer)
+
+  // draw episode and epsillon
+  ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Episode: %v", episode), 0, 20)
+  ebitenutil.DebugPrintAt(screen, fmt.Sprintf("Epsillon: %.2f", eps), 0, 0)
+
+	if ebiten.IsDrawingSkipped() {
+		return nil
+	}
+
+	return nil
 }
 
-func initGrid(){
-  grid = Grid{}
-  //init grid
-  for r:=0; r < screenHeight/tileSize; r++ {
-    for c:=0; c < screenWidth/tileSize; c++{
-      grid.tiles = append(grid.tiles, Tile{r,c,0})
-    }
-  }
+func initGrid() {
+	grid = Grid{}
+	//init grid
+	for r := 0; r < screenHeight/tileSize; r++ {
+		for c := 0; c < screenWidth/tileSize; c++ {
+			grid.tiles = append(grid.tiles, Tile{r, c, 0})
+		}
+	}
 
-  // simple chesse -> reward
-  grid.tiles[2].value = 1
-  grid.tiles[10].value = 1
-  grid.tiles[14].value = 1
-  grid.tiles[12].value = 1
+	//  chesse -> reward
+  grid.tiles[15].value = 1
+  grid.tiles[17].value = 1
+  grid.tiles[19].value = 1
+  grid.tiles[23].value = 1
 
-  // mouse Trap -> punish
-  grid.tiles[3].value = 2
-  grid.tiles[5].value = 2
-  grid.tiles[7].value = 2
-  grid.tiles[11].value = 2
-  grid.tiles[22].value = 2
-  grid.tiles[27].value = 2
-  grid.tiles[32].value = 2
+	// mouse Trap -> punish
+	grid.tiles[3].value = 2
+	grid.tiles[5].value = 2
+	grid.tiles[7].value = 2
+	grid.tiles[22].value = 2
+	grid.tiles[32].value = 2
 
-  // tripple Cheese -> end of episode
-  grid.tiles[35].value = 3
+	// tripple Cheese -> end of episode
+	grid.tiles[35].value = 3
 
 }
 
 func main() {
-
-  player = mouse{0,0,none}
-  player.reset()
+	agent = NewAgent(4)
+	player = mouse{0, 0, none}
+	player.reset()
+  ebiten.SetMaxTPS(30)
 	if err := ebiten.Run(update, screenWidth, screenHeight, 1, "MouseCheese AI"); err != nil {
 		log.Fatal(err)
 	}
